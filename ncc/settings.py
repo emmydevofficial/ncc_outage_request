b6b40229a43c94dd2dabec32ef4bf945688b3411
Apple <emmydevofficial@gmail.com>
2026-09-22 13:33:31 +0100

    Migrated to Postgres

--- ncc/settings.py
+++ ncc/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'ncc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ncc_req',
+        'USER': 'ncc_req',
+        'PASSWORD': 'ncc12345678',
+        'HOST': 'postgresql-208808-0.cloudclusters.net',
+        'PORT': '10022',
     }
 }
 
